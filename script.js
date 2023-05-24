57ac4b27e6f9fb2cc6169c627f8d5cba5060d484
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-05-24 13:04:12 +0200

    added JSDoc

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 
-
 window.addEventListener('DOMContentLoaded', () => {
     const display = document.querySelector('input[name="display"]');
     const buttons = document.querySelectorAll('.calculator input[type="button"]');
-  
+    
     buttons.forEach(button => {
       button.addEventListener('click', () => {
         const value = button.value;
@@ -11,6 +10,11 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     });
   
+    /**
+     * Handles button clicks and performs the corresponding action based on the value of the button.
+     * @param {string} value - The value of the button that was clicked.
+     * 
+     */
     function handleButtonClick(value) {
       switch (value) {
         case 'AC':
@@ -26,19 +30,32 @@ window.addEventListener('DOMContentLoaded', () => {
           appendToDisplay(value);
       }
     }
-  
+
+    /**
+     * Clears the display
+     * @returns {void} 
+     */
     function clearDisplay() {
       display.value = '';
     }
-  
+
+    /**
+     * Deletes the last entry
+     * @returns {void}
+     */
     function deleteLastEntry() {
       display.value = display.value.toString().slice(0, -1);
     }
-  
+    
+
+    /** 
+     * @param {string} value The value of the button that was clicked is appended to the display
+     */
     function appendToDisplay(value) {
       display.value += value;
     }
-  
+   
+    // Evaluates the expression
     function evaluateExpression() {
       display.value = eval(display.value);
     }
